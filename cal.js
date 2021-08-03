1fa38d6f51ff9ea588e6caa85bae7d738b03f95d
nakashi-san <nakashi-color@users.noreply.github.com>
2021-08-04 00:16:23 +0900

    Update cal.js

--- cal.js
+++ cal.js
@@ -20,8 +20,8 @@ calObj[0].daysClass = new Object();
 // 2012/6/9にBirthdayクラスを指定ってことね(和田の誕生日です)。
 // セミコロンで区切ると説明コメント、URLを付けることができます(全クラス指定共通)。
 // calObj[0].daysClass["6/9"] = 'Birthday;和田憲幸の誕生日だゾ☆<br />年を抜くと毎年。;http://weblogs.trancedive.com';
-calObj[0].daysClass["8/4"] = 'Holyday;定休日';
-calObj[0].daysClass["8/25"] = 'Holyday;定休日';
+calObj[0].daysClass["8/24"] = 'Holyday;定休日';
+calObj[0].daysClass["8/31"] = 'Holyday;定休日';
 
 
 // ["xDaysLater"] ○日後のクラス指定
@@ -66,7 +66,7 @@ calObj[0].click = false;
 calObj[0].clickClassName = "";
 
 // 優先度 クラス指定する順番が変わります。
-calObj[0].priority = new Array('week','xDay','xDaysLater','day','backward');
+calObj[0].priority = new Array('week', 'xDay', 'xDaysLater', 'day', 'backward');
 
 //【カレンダー0の設定はここまで】
 
@@ -74,9 +74,9 @@ calObj[0].priority = new Array('week','xDay','xDaysLater','day','backward');
 
 // 以下、さわらぬ神にたたりなし
 calObj.calendars = new Array();
-calObj.days = new Array(0,31,28,31,30,31,30,31,31,30,31,30,31);
-calObj.weekName = new Array("日","月","火","水","木","金","土");
-calObj.monthName = new Array('','1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月');
+calObj.days = new Array(0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+calObj.weekName = new Array("日", "月", "火", "水", "木", "金", "土");
+calObj.monthName = new Array('', '1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月');
 calObj.date = new Date();
 calObj.date = new Date(calObj.date.getFullYear() + "/" + (calObj.date.getMonth() + 1) + "/" + calObj.date.getDate() + " 00:00:00");
 calObj.day = calObj.date.getDate();
@@ -84,23 +84,23 @@ calObj.month = calObj.date.getMonth() + 1;
 calObj.year = calObj.date.getFullYear();
 calObj.currentList = null;
 
-function cal_init(){
+function cal_init() {
 	var d = window.document;
 	var tagObj = d.getElementsByTagName("div");
 	var calToday = new Date();
-	for(var i=0;i<tagObj.length;i++){
-		if(tagObj[i].className == "cal_wrapper"){
-			var calId = Number(tagObj[i].id.substring(3,tagObj[i].id.length));
+	for (var i = 0; i < tagObj.length; i++) {
+		if (tagObj[i].className == "cal_wrapper") {
+			var calId = Number(tagObj[i].id.substring(3, tagObj[i].id.length));
 			calObj.calendars.push(calId);
-			if(0 < (calObj.month + calObj[calId].defaultMonth) && (calObj.month + calObj[calId].defaultMonth) < 13)
-				calObj[calId].currentMonth = new Date(calObj.year+"/"+(calObj.month + calObj[calId].defaultMonth)+"/"+"1 00:00:00");
+			if (0 < (calObj.month + calObj[calId].defaultMonth) && (calObj.month + calObj[calId].defaultMonth) < 13)
+				calObj[calId].currentMonth = new Date(calObj.year + "/" + (calObj.month + calObj[calId].defaultMonth) + "/" + "1 00:00:00");
 			else
-				calObj[calId].currentMonth = new Date((calObj.year+1)+"/"+((calObj.month + calObj[calId].defaultMonth)%12)+"/"+"1 00:00:00");
+				calObj[calId].currentMonth = new Date((calObj.year + 1) + "/" + ((calObj.month + calObj[calId].defaultMonth) % 12) + "/" + "1 00:00:00");
 			cal_create(calId);
 		}
 	}
 }
-function cal_create(calId){
+function cal_create(calId) {
 	var d = window.document;
 	var day = calObj[calId].currentMonth.getDate();
 	var month = calObj[calId].currentMonth.getMonth() + 1;
@@ -110,14 +110,14 @@ function cal_create(calId){
 	var bisDay = 0;
 	var MonthDays = calObj.days[month];
 	var WeekCnt = new Array();
-	if(month == 2){
-		if(year % 100 == 0 || year % 4 != 0){
-			if(year % 400 != 0)
+	if (month == 2) {
+		if (year % 100 == 0 || year % 4 != 0) {
+			if (year % 400 != 0)
 				bisDay = 0;
 			else
 				bisDay = 1;
 		}
-		else if(year % 4 == 0)
+		else if (year % 4 == 0)
 			bisDay = 1;
 		else
 			bisDay = 0;
@@ -126,21 +126,21 @@ function cal_create(calId){
 	var calHTML = "<table border='0' cellspacing='0' cellpadding='0' class='cal'>";
 	calHTML += "<tr><th colspan='7'>";
 	calHTML += "<div class='cal_ui'>";
-	calHTML += "<input type='button' onclick='cal_move("+calId+",-1);' value='&lt; prev' />";
-	calHTML += "<input type='button' onclick='cal_move("+calId+",null);' value='-' />";
-	calHTML += "<input type='button' onclick='cal_move("+calId+",1);' value='next &gt;' />";
+	calHTML += "<input type='button' onclick='cal_move(" + calId + ",-1);' value='&lt; prev' />";
+	calHTML += "<input type='button' onclick='cal_move(" + calId + ",null);' value='-' />";
+	calHTML += "<input type='button' onclick='cal_move(" + calId + ",1);' value='next &gt;' />";
 	calHTML += "</div>";
 	calHTML += "<p>" + year + "年" + calObj.monthName[month] + "</p></th></tr>";
 	calHTML += "<tr class='headline'>";
-	for(var i=0;i<calObj.weekName.length;i++)
+	for (var i = 0; i < calObj.weekName.length; i++)
 		calHTML += "<td>" + calObj.weekName[i] + "</td>";
 	calHTML += "</tr><tr>";
-	for(var i=0;i<week;i++)
+	for (var i = 0; i < week; i++)
 		calHTML += "<td>&nbsp;</td>";
-	for(dayCnt=1;dayCnt<=(calObj.days[month]+bisDay);dayCnt++){
+	for (dayCnt = 1; dayCnt <= (calObj.days[month] + bisDay); dayCnt++) {
 		var dayStr = year + "/" + month + "/" + dayCnt;
 		var dayStrN = month + "/" + dayCnt;
-		if(WeekCnt[week] == undefined)
+		if (WeekCnt[week] == undefined)
 			WeekCnt[week] = 0;
 		WeekCnt[week]++;
 
@@ -154,36 +154,36 @@ function cal_create(calId){
 		var dayClassText = new Object();
 		var currentDayDate = new Date(year + "/" + month + "/" + dayCnt + " 00:00:00");
 		var laterDay = Math.floor((currentDayDate.getTime() - calObj.date.getTime()) / 1000 / (60 * 60 * 24));
-		var tdId = "td_"+calId+"_"+year+"_"+month+"_"+dayCnt;
+		var tdId = "td_" + calId + "_" + year + "_" + month + "_" + dayCnt;
 
 		// backward
-		if(calObj[calId].backward != null && currentDayDate.getTime() < calObj.date.getTime())
+		if (calObj[calId].backward != null && currentDayDate.getTime() < calObj.date.getTime())
 			dayClass["backward"] = calObj[calId].backward;
 
 		// week
-		if(calObj[calId].month[weekStr] != undefined)
+		if (calObj[calId].month[weekStr] != undefined)
 			dayClass["week"] = calObj[calId].month[weekStr];
-		else if(calObj[calId].month[monStr] != undefined)
+		else if (calObj[calId].month[monStr] != undefined)
 			dayClass["week"] = calObj[calId].month[monStr];
-		else if(calObj[calId].week[week] != undefined){
-			if(typeof(calObj[calId].week[week]) == "object" && calObj[calId].week[week][WeekCnt[week]] != undefined){
+		else if (calObj[calId].week[week] != undefined) {
+			if (typeof (calObj[calId].week[week]) == "object" && calObj[calId].week[week][WeekCnt[week]] != undefined) {
 				dayClass["week"] = calObj[calId].week[week][WeekCnt[week]];
 			}
-			else if(calObj[calId].week[week] != undefined && typeof(calObj[calId].week[week]) != "object")
+			else if (calObj[calId].week[week] != undefined && typeof (calObj[calId].week[week]) != "object")
 				dayClass["week"] = calObj[calId].week[week];
 		}
 		// xDay
-		if(calObj[calId].xDays[dayCnt] != undefined)
+		if (calObj[calId].xDays[dayCnt] != undefined)
 			dayClass["xDay"] = calObj[calId].xDays[dayCnt];
 
 		// xDaysLater
-		if(calObj[calId].xDaysLater[laterDay] != undefined)
+		if (calObj[calId].xDaysLater[laterDay] != undefined)
 			dayClass["xDaysLater"] = calObj[calId].xDaysLater[laterDay];
 
 		// day
-		if(calObj[calId].daysClass[dayStr] != undefined)
+		if (calObj[calId].daysClass[dayStr] != undefined)
 			dayClass["day"] = calObj[calId].daysClass[dayStr];
-		else if(calObj[calId].daysClass[dayStrN] != undefined)
+		else if (calObj[calId].daysClass[dayStrN] != undefined)
 			dayClass["day"] = calObj[calId].daysClass[dayStrN];
 
 		var tdClassArr = new Array();
@@ -193,135 +193,135 @@ function cal_create(calId){
 		var tdTextStr = "";
 		var tdMouse = "";
 		var tdClassNames = new Object();
-		for(var ci=0;ci<calObj[calId].priority.length;ci++){
-			if(dayClass[calObj[calId].priority[ci]] != undefined){
+		for (var ci = 0; ci < calObj[calId].priority.length; ci++) {
+			if (dayClass[calObj[calId].priority[ci]] != undefined) {
 				var splitArr = new Array();
 				splitArr = dayClass[calObj[calId].priority[ci]].split(';');
 				tdClassArr.push(splitArr[0]);
 				tdClassNames[splitArr[0]] = true;
-				if(splitArr[1] != undefined){
+				if (splitArr[1] != undefined) {
 					tdTextArr.push(splitArr[1]);
 					var tdTextListLink = "";
-					if(splitArr[2] != undefined)
-						tdTextListLink = " onclick=\"cal_open(\'"+splitArr[2]+"\')\"";
-					tdTextListArr.push('<ol><li id="'+tdId+'_li" onmouseover="cal_list2day_over(this)" onmouseout="cal_list2day_out(this)" value="'+dayCnt+'"'+tdTextListLink+'>'+splitArr[1]+'</li></ol>');
+					if (splitArr[2] != undefined)
+						tdTextListLink = " onclick=\"cal_open(\'" + splitArr[2] + "\')\"";
+					tdTextListArr.push('<ol><li id="' + tdId + '_li" onmouseover="cal_list2day_over(this)" onmouseout="cal_list2day_out(this)" value="' + dayCnt + '"' + tdTextListLink + '>' + splitArr[1] + '</li></ol>');
 				}
-				if(splitArr[2] != undefined)
+				if (splitArr[2] != undefined)
 					tdLinkArr.push(splitArr[2]);
 			}
 		}
-		if(tdTextArr.length > 0){
-			tdTextStr = "<span id='"+tdId+"'>";
-			for(var i=0;i<tdTextArr.length;i++){
+		if (tdTextArr.length > 0) {
+			tdTextStr = "<span id='" + tdId + "'>";
+			for (var i = 0; i < tdTextArr.length; i++) {
 				tdTextStr += tdTextArr[i] + "<br />";
 			}
 			tdTextStr += "</span>";
-			tdMouse = " onmouseover=\"cal_disp_text(\'"+tdId+"\')\" onmouseout=\"cal_hide_text(\'"+tdId+"\')\"";
+			tdMouse = " onmouseover=\"cal_disp_text(\'" + tdId + "\')\" onmouseout=\"cal_hide_text(\'" + tdId + "\')\"";
 			tdClassArr.push('pointer');
 		}
-		if(tdLinkArr.length > 0)
-			tdMouse += " onclick=\"cal_open(\'"+tdLinkArr[0]+"\')\"";
-		else if(calObj[calId].click){
+		if (tdLinkArr.length > 0)
+			tdMouse += " onclick=\"cal_open(\'" + tdLinkArr[0] + "\')\"";
+		else if (calObj[calId].click) {
 			var clickOpenURI = calObj[calId].clickURI;
-			clickOpenURI = clickOpenURI.replace(/_YEAR_/ig,year);
-			clickOpenURI = clickOpenURI.replace(/_MONTH_/ig,month);
-			clickOpenURI = clickOpenURI.replace(/_DAY_/ig,dayCnt);
-			if((calObj[calId].clickClassName != "" && tdClassNames[calObj[calId].clickClassName]) || calObj[calId].clickClassName == ""){
-				tdMouse += " onclick=\"cal_open(\'"+clickOpenURI+"\')\"";
+			clickOpenURI = clickOpenURI.replace(/_YEAR_/ig, year);
+			clickOpenURI = clickOpenURI.replace(/_MONTH_/ig, month);
+			clickOpenURI = clickOpenURI.replace(/_DAY_/ig, dayCnt);
+			if ((calObj[calId].clickClassName != "" && tdClassNames[calObj[calId].clickClassName]) || calObj[calId].clickClassName == "") {
+				tdMouse += " onclick=\"cal_open(\'" + clickOpenURI + "\')\"";
 				tdClassArr.push('pointer');
 			}
 		}
-		if(tdClassArr.length > 0)
+		if (tdClassArr.length > 0)
 			tdClassStr = " class='" + tdClassArr.join(' ') + "'";
-		calHTML += "<td id='"+tdId+"_td'><div"+tdClassStr+tdMouse+">" + dayCnt + tdTextStr + "</div></td>";
-		if(week == 6){
+		calHTML += "<td id='" + tdId + "_td'><div" + tdClassStr + tdMouse + ">" + dayCnt + tdTextStr + "</div></td>";
+		if (week == 6) {
 			calHTML += "</tr>";
-			if(dayCnt < calObj.days[month])
+			if (dayCnt < calObj.days[month])
 				calHTML += "<tr>";
 			week = 0;
 		}
 		else
 			week++;
 	}
-	while(week <= 6 && week != 0){
+	while (week <= 6 && week != 0) {
 		calHTML += "<td>&nbsp;</td>";
-		if(week == 6)
+		if (week == 6)
 			calHTML += "</tr>";
 		week++;
 	}
 	calHTML += "</table>";
-	d.getElementById('cal'+calId).innerHTML = calHTML;
+	d.getElementById('cal' + calId).innerHTML = calHTML;
 
 	// list
-	if(d.getElementById('schedule'+calId)){
-		d.getElementById('schedule'+calId).innerHTML = "";
-		if(tdTextListArr.length > 0 && d.getElementById('schedule'+calId)){
-			d.getElementById('schedule'+calId).innerHTML = tdTextListArr.join('');
+	if (d.getElementById('schedule' + calId)) {
+		d.getElementById('schedule' + calId).innerHTML = "";
+		if (tdTextListArr.length > 0 && d.getElementById('schedule' + calId)) {
+			d.getElementById('schedule' + calId).innerHTML = tdTextListArr.join('');
 		}
 	}
 	// /list
 }
-function cal_list2day_over(obj){
+function cal_list2day_over(obj) {
 	var d = window.document;
-	var dayId = obj.id.substring(0,obj.id.indexOf('_li'));
-	if(d.getElementById(calObj.currentList))
+	var dayId = obj.id.substring(0, obj.id.indexOf('_li'));
+	if (d.getElementById(calObj.currentList))
 		d.getElementById(calObj.currentList).style.backgroundColor = '#FFF';
-	calObj.currentList = dayId+'_td';
-	if(d.getElementById(dayId+'_td'))
-		d.getElementById(dayId+'_td').style.backgroundColor = '#CCC';
+	calObj.currentList = dayId + '_td';
+	if (d.getElementById(dayId + '_td'))
+		d.getElementById(dayId + '_td').style.backgroundColor = '#CCC';
 }
-function cal_list2day_out(obj){
+function cal_list2day_out(obj) {
 	var d = window.document;
-	var dayId = obj.id.substring(0,obj.id.indexOf('_li'));
-	if(d.getElementById(calObj.currentList))
+	var dayId = obj.id.substring(0, obj.id.indexOf('_li'));
+	if (d.getElementById(calObj.currentList))
 		d.getElementById(calObj.currentList).style.backgroundColor = '#FFF';
 }
-function cal_open(uri){
+function cal_open(uri) {
 	window.open(uri);
 }
-function cal_disp_text(textId){
+function cal_disp_text(textId) {
 	var d = window.document;
-	if(navigator.userAgent.indexOf('MSIE') == -1)
+	if (navigator.userAgent.indexOf('MSIE') == -1)
 		d.getElementById(textId).style.display = "block";
 }
-function cal_hide_text(textId){
+function cal_hide_text(textId) {
 	var d = window.document;
 	d.getElementById(textId).style.display = "none";
 }
 
-function cal_move(calId,m){
-	if(m == null)
-		calObj[calId].currentMonth = new Date(calObj.year+"/"+(calObj.month)+"/"+"1 00:00:00");
+function cal_move(calId, m) {
+	if (m == null)
+		calObj[calId].currentMonth = new Date(calObj.year + "/" + (calObj.month) + "/" + "1 00:00:00");
 	else {
 		var day = calObj[calId].currentMonth.getDate();
 		var month = calObj[calId].currentMonth.getMonth() + 1;
 		var year = calObj[calId].currentMonth.getFullYear();
-		if(0 < month + m && month + m < 13)
-			calObj[calId].currentMonth = new Date(year+"/"+(month + m)+"/"+"1 00:00:00");
-		else if((month + m) < 1){
+		if (0 < month + m && month + m < 13)
+			calObj[calId].currentMonth = new Date(year + "/" + (month + m) + "/" + "1 00:00:00");
+		else if ((month + m) < 1) {
 			year--;
 			month = 12;
-			calObj[calId].currentMonth = new Date(year+"/"+(month)+"/"+"1 00:00:00");
+			calObj[calId].currentMonth = new Date(year + "/" + (month) + "/" + "1 00:00:00");
 		}
 		else {
 			year++;
 			month = 1;
-			calObj[calId].currentMonth = new Date(year+"/"+(month)+"/"+"1 00:00:00");
+			calObj[calId].currentMonth = new Date(year + "/" + (month) + "/" + "1 00:00:00");
 		}
 	}
 	cal_create(calId);
 }
 function cal_clone(obj) {
 	var dest;
-	if(typeof obj == 'object'){
-		if(obj instanceof Array){
+	if (typeof obj == 'object') {
+		if (obj instanceof Array) {
 			dest = new Array();
-			for(i=0;i<obj.length;i++)
+			for (i = 0; i < obj.length; i++)
 				dest[i] = cal_clone(obj[i]);
 		}
 		else {
 			dest = new Object();
-			for(prop in obj)
+			for (prop in obj)
 				dest[prop] = cal_clone(obj[prop]);
 		}
 	}
@@ -329,7 +329,7 @@ function cal_clone(obj) {
 		dest = obj;
 	return dest;
 }
-function cal_getMonth(){
+function cal_getMonth() {
 
 }
 cal_init();
